ef2f741c7329eaafdbeb6307ab304b022aec4b81
shubhamvts6 <shubhamvts6@gmail.com>
2023-04-10 12:37:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+// INSTRUCTIONS
+// Debug the code
+// Question 1
+// let launch = false;
+// let Level = 17000;
+
+// if (Level >= 20000 {
+//    console.log('Fuel level cleared.');
+//    launch = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launch = false;
+// }
+
+
+// Debug the code
+
+// Question 1
+
+// let launch = false;
+// let level = 17000;
+
+// if (level >= 20000) {
+//    console.log('Fuel level cleared.');
+//    launch = true;
+// } else {
+//    console.log('WARNING: Insufficient fuel!');
+//    launch = false;
+// }
+
+// Question 2
+// let launch = false;
+// let crew = true;
+// let computer = 'green';
+
+// if (crew &&& computer === 'green') {
+//    console.log('Crew & computer cleared.');
+//    launch = true;
+// } else {
+//    console.log('WARNING: Crew or computer not ready!');
+//    launch = false;
+// }
+
+// if (launch) {
+//    console.log(("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+//    console.log("Fed parrot...");
+//    console.log("Ignition...");
+//    console.log("Liftoff!");
+// } else {
+//    console.log("Launch scrubbed.");
+// }
+
+/// soluction
+let launch = false;
+let crew = true;
+let computer = 'green';
+
+if (crew && computer === 'green') {
+   console.log('Crew & computer cleared.');
+   launch = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launch = false;
+}
+
+if (launch) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+
+//the console output is missing closing parentheses. 
+let launchReady = false;
+let fuellevel = 17000;
+
+if (fuellevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+// fuelLevel
